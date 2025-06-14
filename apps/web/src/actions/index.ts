417b45ca80fbead59c4eac4e7b6da0652480d39d
Raphael Santos <rvsantos97@gmail.com>
2025-06-14 02:30:50 -0300

    chore: changed static api url for env

--- apps/web/src/actions/index.ts
+++ apps/web/src/actions/index.ts
@@ -7,9 +7,7 @@ export const server = {
       value: z.string(),
     }),
     handler: async (input) => {
-      const data = await fetch(
-        'http://localhost:3000/api/'.concat(input.value)
-      );
+      const data = await fetch(import.meta.env.API_URL.concat(input.value));
       if (!data) return { message: 'spell not found' };
 
       const spell = await data.json();
