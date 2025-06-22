10d3f8891436e06fe0f342eec83dbe30c75eff7b
Raphael Santos <rvsantos97@gmail.com>
2025-06-22 17:44:56 -0300

    refactor: removed astro/node and configured astro/vercel on astro.config file

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,16 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
-
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
