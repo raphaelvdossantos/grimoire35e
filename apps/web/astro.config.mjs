eac3d6266a88456b0a1653976b0b41c112c049f8
Raphael Santos <rvsantos97@gmail.com>
2025-06-22 21:39:29 -0300

    chore: added sitemap to project

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
+  site: 'https://dnd35api.com',
 });
